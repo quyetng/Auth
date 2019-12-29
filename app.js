7fbf233de18a5f471f66b05a6b7599f782297ed6
QuyetNguyen <quyetnguyentan@gmail.com>
2019-12-29 11:09:41 -0700

    Add hash

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
 const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 
 
 const app = express();
@@ -26,7 +27,7 @@ const userSchema = new mongoose.Schema({
 });
 
 //var secret = "Thisisasecretstring";
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password']});
+//userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password']});
 
 const User = mongoose.model("User", userSchema);
 
@@ -45,7 +46,7 @@ app.get("/login", function(req, res){
 app.post("/register", function(req, res){
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
 
   });
 
@@ -60,7 +61,7 @@ app.post("/register", function(req, res){
 
 app.post("/login", function(req, res){
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({email: username}, function(err, foundUser){
     if(err) {
